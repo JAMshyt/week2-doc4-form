1680cd591c5e5dfe16406e711be9363a6389d655
JAMshyt <77636014+JAMshyt@users.noreply.github.com>
2022-12-08 12:39:43 +0300

    Add files via upload

---
+++ Форма рекурсии 2/Форма рекурсии 2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Форма_рекурсии_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static void Conv(string check, ref int num)
+        {
+            if (check != "")
+            {
+                if (check[0] == '1' | check[0] == '0')
+                {
+                    num = num * 2 + (check[0] == '1' ? 1 : 0);
+                    check = check.Substring(1);
+                    Conv(check, ref num);
+                }
+                else
+                {
+                    MessageBox.Show("Вы ввели не двоичное");
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int num = 0, c;
+            string check = "";
+            do
+            {
+                try
+                {
+                    c = Convert.ToInt32(textBox1.Text);
+                    check = Convert.ToString(c);
+                    break;
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка");
+                    break;
+                }
+            } while (true);
+            Conv(check, ref num);
+            textBox2.Text = "Результат: " + num;
+        }
+    }
+}
---
+++ Форму рекусрии 1/Форму рекусрии 1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Форму_рекусрии_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static double calc(int n, double x, int res = 1)
+        {
+            if (res > n) return x;
+            return x / (res + calc(n, x, res + 1));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = 0;
+            double x = 0;
+            do
+            {
+                try
+                {
+                    n = Convert.ToInt32(textBox1.Text);
+                    if (n < 0)
+                    {
+                        MessageBox.Show("Вы ввели n менье 0");
+                        break;
+                    }
+                    else
+                    {
+                        x = Convert.ToDouble(textBox2.Text);
+                        break;
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка");
+                    break;
+                }
+            } while (true);
+
+            textBox3.Text = "Результат: " + calc(n, x);
+        }
+    }
+}
